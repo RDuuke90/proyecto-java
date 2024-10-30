346ec641c463b091e0f92a47725f2cc975033330
Samuel Sml <veramirandasamuel6@gmail.com>
2024-10-30 16:34:31 -0500

    feat: create main class

--- Main.java
+++ Main.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        CalculadoraSimple calc = new CalculadoraSimple();
+
+        int resultadoSuma = calc.sumar(10, 5);
+        int resultadoMultiplicacion = calc.multiplicar(10, 10);
+
+        System.out.println("Resultado de la suma: " + resultadoSuma);
+        System.out.println("Resultado de la multiplicación: " + resultadoMultiplicacion);
+    }
 }
